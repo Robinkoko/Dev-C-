3da4250e311fd4dae93b70562eea94981bd40b91
Robinkoko <161907448+Robinkoko@users.noreply.github.com>
2024-09-14 11:52:51 +0900

    9/14 C

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(num1,num2) 
+{
+	scanf("%d %d",&num1,&num2);
+	printf("%d\n",num1 * (num2 - 380));
+	printf("%d\n",num1 * (num2 -305) / 10);
+	printf("%d\n",num1 * (num2 - 85)/100);
+	printf("%d",num1 * num2);
+}

8c2a7ca49e5394953ec05f6e52778e8596e34994
Robinkoko <161907448+Robinkoko@users.noreply.github.com>
2024-09-28 14:49:24 +0900

    09/28
    09/28 Dev C++

---
+++ 0928.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+//int main() {
+//	
+//	printf("%d월 %d일\n", 9,28);
+//	printf("%d시 %d분\n", 1,15);
+//	printf("%d cm²\n" , 6);
+//	printf("%s 은(는) 사람이다", "홍길동");
+//	return 0;
+//}
+//----------------------------------------------------------------------------------------------------------------------------------------
+//int main() {
+//	int A;
+//	float F1;
+//	char CH;
+//	
+//	A = 15;
+//	F1 = 123.456;
+//	CH = 'A';
+//	
+//	printf("%d\n", A);
+//	printf("%f\n", F1);
+//	printf("%c\n", CH);
+//	return 0;
+//}
+//-------------------------------------------------------------------------------------------------
+//int main() {
+//	int grade = 1;
+//	int classs = 4;
+//	printf("%d학년 %d반\n",grade,classs);
+//	int a = 107;
+//	int b = 1604;
+//	printf("%d동 %d호\n",a,b);
+//	float height = 173.0;
+//	float weight = 55.2;
+//	printf("키: %.2f,몸무게 :%.2f\n",height,weight);
+//	int cir = 10;
+//	float r = 12.45;
+//	printf("원의 중심 : %d, 원의 반지름 : %.2f",cir,r);
+//	return 0;
+//}
+//----------------------------------------------------------------------------------------------------
+//int main() {
+//	int num;
+//	printf("num = /주소 %x\n",&num);
+//	printf("숫자를 입력해 주세요:");
+//	scanf("%d",&num);
+//	printf("입력한 숫자 %d\n",num); 
+//	return 0;
+//}
+//-------------------------------------------------------------------------------------------------------
+int main(){
+	int num;
+	scanf("%d", &num);
+	printf("%d\n",num);
+	printf("num = %d\n",num);
+	return 0;
+}
